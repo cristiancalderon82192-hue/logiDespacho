a472be8fe8a9163fbb6fafec4bc79acf8363951a
cristiancalderon82192-hue <cristian_calderon82192@elpoli.edu.co>
2026-05-29 08:18:20 -0500

    implementacion pwa

--- logistica-despacho/vite.config.js
+++ logistica-despacho/vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        id: '/logidespacho/',
+        name: 'Logística y Despacho',
+        short_name: 'LogiDespacho',
+        description: 'Aplicación de Logística y Despacho',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'logo_it.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    port: 5174,
+    strictPort: true,
+  }
 })
