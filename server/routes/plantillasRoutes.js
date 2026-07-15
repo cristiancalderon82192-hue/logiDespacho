90e1b5c66516419d3a995949637e76401357972a
cristiancalderon82192-hue <cristian_calderon82192@elpoli.edu.co>
2026-07-15 13:16:09 -0500

    Sin ia para lector de pdfs todo en local 2.0

--- server/routes/plantillasRoutes.js
+++ server/routes/plantillasRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const plantillasController = require('../controllers/plantillasController');
-const { verifyToken } = require('../middlewares/authMiddleware'); // Si usan middleware de auth, habría que verificar. Asumiré que no lo fuerzan en todas partes, o si sí, lo aplican en index.js. Por seguridad, expondré las rutas base.
 
 router.get('/plantillas', plantillasController.getPlantillas);
 router.get('/plantillas/:id', plantillasController.getPlantillaById);
